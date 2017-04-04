599db23344e52d0c4a374ad4f5f76e0116d3c50a
Jakub Jankiewicz <jcubic@onet.pl>
2017-04-04 21:59:45 +0200

    redirect to notes when on index and logged in

--- app/components/main/main.controller.js
+++ app/components/main/main.controller.js
@@ -1,5 +1,13 @@
 import svg from './notes.svg';
 
-export default function(rpc) {
+function mainController($state, auth) {
     this.svg = svg;
+    auth.authenticated().then((authenticated) => {
+        if (authenticated) {
+            $state.go('notes');
+        }
+    });
 }
+mainController.$inject = ['$state', 'auth'];
+
+export default mainController;
